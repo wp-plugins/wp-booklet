0dd12312c16e749c3e4b7fc3ce43278c5acabee0
binarystash01 <binarystash01@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-24 07:36:05 +0000

    -Must redo tag
    git-svn-id: https://plugins.svn.wordpress.org/wp-booklet@1166536 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/2.0.1/themes/admin/default/import-nag.php
+++
@@ -1,1 +0,0 @@
-<div class='error wp-booklet2-import-nag'>Don't panic, your old booklets are <b>NOT</b> gone. Click <a href="<?php get_admin_url() ?>edit.php?post_type=wp-booklet2&page=wp-booklet2-import">here</a> to restore them.</div>
